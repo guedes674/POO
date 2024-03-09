bbade85c0a5da88ba6ad4b0d42a1613306a1924e
guedes674 <tiago.matos.guedes555@gmail.com>
2024-03-09 11:30:09 +0000

    a realizar a ficha 3.

--- fichas/ficha3/Circulov2.java
+++ fichas/ficha3/Circulov2.java
@@ -1,5 +1,7 @@
 package ficha3;
 
+import java.lang.StringBuilder;
+
 public class Circulov2 {
 
     //Variáveis de instância
@@ -28,6 +30,44 @@ public class Circulov2 {
     public Circulov2(Circulov2 c2){
         this.ponto = c2.getPonto();
         this.raio = c2.getRaio();
+    }
+
+    /**
+     * Método que verifica se dois Círculosv2 são iguais.
+     * @param o objeto a comparar.
+     * @return boolean que verifica se o objeto que recebeu a mensagem é igual ao que se está a comparar.
+     */
+    public boolean equals(Object o){
+        if(this == o)
+            return true;
+        if((o == null) || (o.getClass() != this.getClass()))
+            return false;
+
+        Circulov2 c = (Circulov2) o;
+
+        return (this.getPonto() == c.getPonto() && this.getRaio() == c.getRaio());
+    }
+
+    /**
+     * Método que devolve o Círculov2 em formato de String.
+     * @return String com as características deste Círculov2.
+     */
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Coordenada do x: ").append(this.getPonto().getX()).append("\n")
+                .append("Coordenada do y: ").append(this.getPonto().getY()).append("\n")
+                .append("Raio: ").append(this.getRaio()).append("\n");
+
+        return sb.toString();
+    }
+
+    /**
+     * Método que representa faz clone de um Círculov2.
+     * @return Círculov2 clone da que recebeu a mensagem.
+     */
+    public Circulov2 clone(){
+        return new Circulov2(this);
     }
 
     public void alteraCentro(int x, int y){
